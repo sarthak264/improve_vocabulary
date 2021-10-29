e2b51a63e5579af761f7dbbdd6ff632da084af8c
sarthak264 <sarthaksharm372@gmail.com>
2021-10-29 12:44:15 +0530

    api is set and working, word meaning are showing

--- src/components/word/Word.jsx
+++ src/components/word/Word.jsx
@@ -1,9 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./word.css";
 
 const Word = () => {
   const [word, setWord] = useState("Loading...");
-  const [meaning, setMeaning] = useState("Meaning would appear here");
+  const [meaning, setMeaning] = useState("");
+
+  const getWord = async () => {
+    const response = await fetch("https://random-words-api.vercel.app/word");
+    const json = await response.json();
+    const data = json[0];
+
+    setWord(data.word);
+    setMeaning(data.definition);
+  };
+
+  useEffect(() => {
+    getWord();
+  }, []);
 
   return (
     <div className="word_container">
